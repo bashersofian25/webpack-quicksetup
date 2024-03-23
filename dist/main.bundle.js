7d7e513a1d65d2cc0897063645853dcd0a09e9cc
bashersofian25 <bsofian4@gmail.com>
2024-03-23 14:02:40 +0300

    almost finished template

---
+++ dist/main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log(working);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxxQiIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stcXVpY2tzZXR1cC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyh3b3JraW5nKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
